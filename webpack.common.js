fc9b40b6a56198f5b0b3bc1e2c84ff559231276f
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-01 20:10:13 -0600

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const cssRules = { test: /\.css$/i, use: ["style-loader", "css-loader"] };
+const htmlRules = { test: /\.html$/i, loader: "html-loader" };
+const assetsRules = { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" };
+const fontsRules = { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource' };
+
+module.exports = {
+  entry: "./src/index.html",
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+
+  module: {
+    rules: [ cssRules, htmlRules, assetsRules, fontsRules ]
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: "eval-source-map",
+
+  devServer: {
+    watchFiles: ["./src/index.html"],
+    client: { overlay: true },
+    open: true,
+    port: 8080,
+    compress: true
+  }
+});
