a3c2f67a22779be1081b7646d0e4e35458233eab
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-01 20:28:59 -0600

    feat: add css minimizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,23 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const miniCssRule = { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] };
+
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+
+  module: { rules: [miniCssRule] },
+
+  optimization: { minimizer: [new CssMinimizerPlugin()] }
 });
