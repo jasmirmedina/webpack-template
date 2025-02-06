0911ceb2fac2a593556a0258d9400806d69a2f3a
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-05 18:12:30 -0600

    feat; add terser webpack plugin and basic conf

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -24,7 +25,19 @@ module.exports = merge(common, {
   },
 
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: { comments: false },
+          compress: { drop_console: true },
+        },
+
+        extractComments: false,
+      }),
+
+      new CssMinimizerPlugin(),
+    ],
+
     splitChunks: { chunks: "all" },
   },
 });
